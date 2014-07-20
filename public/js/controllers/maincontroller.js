6c561a74216e2327e2e42a4c1f8bbc60a9b6e9c8
Soubhik Barari <soubhikbarari@gmail.com>
2014-07-20 13:07:54 -0400

    reorganized dir structure to separate app from api + started controllers

---
+++ public/js/controllers/maincontroller.js
@@ -0,0 +1,8 @@
+var tasaApp = angular.module('tasaApp', []);
+
+tasaApp.controller('mainController', function($scope) {
+	$scope.title = "Tufts Association of South Asians";
+	$scope.abbrv = "TASA";
+	$scope.subtitle = "Where South Asian culture blooms at Tufts.";
+	
+})
