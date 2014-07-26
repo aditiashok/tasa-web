b0522787fde28abfb303cd3f92e809dd0cf0623b
Soubhik Barari <soubhikbarari@gmail.com>
2014-07-26 14:44:13 -0400

    Stable point for UI multi-page

--- public/js/controllers/feedbackctrl.js
+++ public/js/controllers/feedbackctrl.js
@@ -1,4 +1,5 @@
 
 angular.module('feedbackCtrl', []).controller('feedbackController', ['$scope', function($scope) {
-
+	$scope.title = "Feedback";
+	$scope.subtitle = "How was your experience with us?"
 }]);
