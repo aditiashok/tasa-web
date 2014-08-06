4c7481ed492d5122184977e9fcdf2a1877b03b6f
Soubhik Barari <soubhikbarari@gmail.com>
2014-08-05 23:27:37 -0400

    initial pass at eboard feature

--- public/js/controllers/eboardctrl.js
+++ public/js/controllers/eboardctrl.js
@@ -1,5 +1,13 @@
 
-angular.module('eboardCtrl', []).controller('eboardController', ['$scope', function($scope) {
+angular.module('eboardCtrl', []).controller('eboardController', ['$scope', '$http', function($scope, $http) {
 	$scope.title = "E-board";
 	$scope.subtitle = "Introducing the 2014-15 e-board...";
+	$http.get('/api/eboard', { cache : true })
+		.success(function(data) {
+			$scope.eboard = data;
+			console.log($scope.eboard);
+		})
+		.error(function(data) {
+			console.log("* Error in retrieving TASA eboard info", data);
+		});
 }]);
